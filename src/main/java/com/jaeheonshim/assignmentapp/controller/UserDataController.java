e365a4b0a73abe61a234901cb90e2607c36b6ca4
Jaeheon Shim <git@jaeheonshim.com>
2020-09-17 14:13:51 -0400

    Create endpoint for saving user details

--- src/main/java/com/jaeheonshim/assignmentapp/controller/UserDataController.java
+++ src/main/java/com/jaeheonshim/assignmentapp/controller/UserDataController.java
@@ -1,15 +1,14 @@
 package com.jaeheonshim.assignmentapp.controller;
 
 import com.jaeheonshim.assignmentapp.domain.User;
+import com.jaeheonshim.assignmentapp.domain.UserDetailsDto;
 import com.jaeheonshim.assignmentapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -23,10 +22,28 @@ public class UserDataController {
     public ResponseEntity<User> getUser(@AuthenticationPrincipal UserDetails userDetails) {
         Optional<User> userOptional = userRepository.findByEmailAddress(userDetails.getUsername());
 
-        if(userOptional.isPresent()) {
+        if (userOptional.isPresent()) {
             return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<User> saveUser(@AuthenticationPrincipal UserDetails userDetails, @RequestBody UserDetailsDto userDetailsDto) {
+        Optional<User> userOptional = userRepository.findByEmailAddress(userDetails.getUsername());
+
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        User user = userOptional.get();
+
+        if (userDetailsDto.getName() != null)
+            user.setName(userDetailsDto.getName());
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok(user);
     }
 }
---
+++ src/main/java/com/jaeheonshim/assignmentapp/domain/AssignmentSort.java
@@ -0,0 +1,6 @@
+package com.jaeheonshim.assignmentapp.domain;
+
+public enum AssignmentSort {
+    BY_DUE_DATE,
+    BY_ASSIGN_DATE
+}
